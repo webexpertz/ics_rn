6dd4acfa53d366e72a7291f27016c35f48b4a545
webexpertz <webexpertz>
2021-12-07 18:47:00 +0530

    Initial commit

---
+++ ios/Pods/Target Support Files/Pods-ICSTests/Pods-ICSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ICSTests : NSObject
+@end
+@implementation PodsDummy_Pods_ICSTests
+@end
---
+++ ios/build/ICS.build/Debug-iphonesimulator/ICS.build/DerivedSources/ICS_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char ICSVersionString[];
+ extern const double ICSVersionNumber;
+
+ const unsigned char ICSVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ICS  PROJECT:ICS-1" "\n";
+ const double ICSVersionNumber __attribute__ ((used)) = (double)1.;
